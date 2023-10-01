3d7b593ef033fb28a0d4cf25e40879e9936bb6c4
Sreerag Mohan <sreeraagmohancv@gmail.com>
2023-10-01 13:50:21 +0530

    Added 'Banner' Backend & Updated Styles

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,18 +1,30 @@
+import { useEffect, useState } from 'react'
 import './Banner.css'
+import axios from 'axios'
+import { options } from '../../Constants/Constants'
+import { imageUrl } from '../../Constants/Constants'
 
 function Banner() {
 
-  
+  const [movie, setMovie] = useState()
+
+  useEffect(() => {
+   axios.get('https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&with_genres=horror', options).then( (res) => {
+    console.log(res.data.results[0])
+    setMovie(res.data.results[18])
+   })
+  }, [])
+
   return (
-    <div className='banner'>
+    <div style={{backgroundImage:`url(${movie ? imageUrl+movie.poster_path : "" })`}} className='banner'>
       <div className='content'>
-        <h1 className='title'>Avengers Endgame</h1>
+        <h1 className='title'>{movie ? movie.title : ""}</h1>
         <div className='banner_buttons'>
-            <button className='button' id='play'>Play</button>
+            <button className='button' id='play'>&#9654; Play</button>
             <button className='button'>More Info</button>
         </div>
-        <h1 className='description'>The grave course of events set in motion by Thanos that wiped out half the universe and fractured the Avengers ranks compels the remaining Avengers to take one final stand in Marvel Studios' grand conclusion to..</h1>
-      </div>
+        <h1 className='description'>{movie ? movie.overview : ""}</h1>
+        </div>
       <div className='fade_bottom' ></div>
     </div>
   )
--- src/Constants/Constants.js
+++ src/Constants/Constants.js
@@ -1,5 +1,13 @@
 export const BaseUrl = 'https://api.themoviedb.org/3'
 export const API_KEY = '370ae0ac5e781dad9cb9eb314bbc3fff'
-export const ACCESS_TOKEN ='eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzNzBhZTBhYzVlNzgxZGFkOWNiOWViMzE0YmJjM2ZmZiIsInN1YiI6IjY1MTdkMzZhOTY3Y2M3MDEzOWQ5N2Y3YSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.8o_xhah1A5LtfZ5a6joeDIaNtHEBCtaTMR5eru3garQ'
+export const imageUrl = 'https://image.tmdb.org/t/p/original'
 
 
+
+export const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzNzBhZTBhYzVlNzgxZGFkOWNiOWViMzE0YmJjM2ZmZiIsInN1YiI6IjY1MTdkMzZhOTY3Y2M3MDEzOWQ5N2Y3YSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.8o_xhah1A5LtfZ5a6joeDIaNtHEBCtaTMR5eru3garQ'
+    }
+  };
