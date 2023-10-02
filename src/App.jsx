92e566a6593be6ab50835c15a1d0ca39b2236a6f
Sreerag Mohan <sreeraagmohancv@gmail.com>
2023-10-02 17:32:53 +0530

    Added Search Input

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import NavBar from "./Components/NavBar/NavBar";
 import Banner from "./Components/Banner/Banner";
 import './App.css'
 import RowPost from "./Components/RowPost/RowPost";
-import { Originals, Action, Comedy } from "./urls";
+import { Originals, Action, Comedy, Horror } from "./urls";
 
 
 function App() {
@@ -13,6 +13,7 @@ function App() {
      <Banner/>
      <RowPost url={Originals} title={'Netflix Originals'} />
      <RowPost url={Action} title={'Action'} isSmall />
+     <RowPost url={Horror} title={'Horror'} isSmall />
      <RowPost url={Comedy} title={'Comedy'} isSmall />
     </div>
   );
--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -5,6 +5,8 @@ function NavBar() {
   return (
     <div className='navbar'>
       <img className='logo' src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1920px-Netflix_2015_logo.svg.png" alt="" />
+      <input className='input' type="text" />
+      <button className='btn'>&#x1F50E;&#xFE0E;</button>
       <img className='avatar' src="https://i.pinimg.com/originals/0d/dc/ca/0ddccae723d85a703b798a5e682c23c1.png" alt="" />
     </div>
   )
